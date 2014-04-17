be72d592902d3f97b83dd48dc10470fd254e5c24
Lionel Sambuc <lionel@minix3.org>
2014-04-17 11:36:49 +0200

    devel/glib2

--- devel/glib2/patches/patch-gio_glocalfile.c
+++ devel/glib2/patches/patch-gio_glocalfile.c
@@ -1,11 +1,17 @@
-$NetBSD: patch-gio_glocalfile.c,v 1.3 2013/12/09 14:46:19 wiz Exp $
-
-Fix compilation on Solaris where O_DIRECTORY does not exist.
-PR pkg/48435.
-
---- gio/glocalfile.c.orig	2013-11-12 05:30:22.000000000 +0000
-+++ gio/glocalfile.c
-@@ -2699,7 +2699,11 @@ g_local_file_measure_size_of_file (gint 
+--- ./gio/glocalfile.c.orig	Tue Nov 12 05:30:22 2013
++++ ./gio/glocalfile.c	Fri May  9 12:26:13 2014
+@@ -89,6 +89,10 @@
+ #endif
+ #endif
+ 
++#ifdef __minix
++/* LSC: Not very proud of that one, but... */
++#undef AT_FDCWD
++#endif
+ 
+ static void g_local_file_file_iface_init (GFileIface *iface);
+ 
+@@ -2699,7 +2703,11 @@ g_local_file_measure_size_of_file (gint           pare
          return FALSE;
  
  #ifdef AT_FDCWD
