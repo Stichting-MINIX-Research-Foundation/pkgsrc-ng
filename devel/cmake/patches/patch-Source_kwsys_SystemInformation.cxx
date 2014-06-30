f188dc0d0aaf47609c0edc77de4e684cd744a440
Ben Gras <ben@minix3.org>
2014-06-30 16:18:03 +0000

    devel/cmake

--- devel/cmake/patches/patch-Source_kwsys_SystemInformation.cxx
+++ devel/cmake/patches/patch-Source_kwsys_SystemInformation.cxx
@@ -1,18 +1,12 @@
-$NetBSD: patch-Source_kwsys_SystemInformation.cxx,v 1.7 2014/03/25 07:11:52 asau Exp $
-
-* Add more conditional handling for NetBSD, same as others.
-* Treat FreeBSD and DragonFly the same way as NetBSD and OpenBSD.
-* Treat Solaris same as Linux.
-* Use correct cmake define.
-
---- Source/kwsys/SystemInformation.cxx.orig	2014-01-16 17:15:08.000000000 +0000
-+++ Source/kwsys/SystemInformation.cxx
+$NetBSD$
+--- Source/kwsys/SystemInformation.cxx.orig	Thu Jan 16 17:15:08 2014
++++ Source/kwsys/SystemInformation.cxx	Fri Jul 11 06:22:41 2014
 @@ -78,9 +78,9 @@ typedef int siginfo_t;
  # include <errno.h> // extern int errno;
  #endif
  
 -#ifdef __FreeBSD__
-+#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__minix) || defined(__FreeBSD__) || defined(__DragonFly__)
 +# include <sys/param.h>
  # include <sys/sysctl.h>
 -# include <fenv.h>
@@ -49,7 +43,19 @@ $NetBSD: patch-Source_kwsys_SystemInformation.cxx,v 1.7 2014/03/25 07:11:52 asau
  # include <fenv.h>
  # include <sys/socket.h>
  # include <netdb.h>
-@@ -4616,7 +4620,7 @@ bool SystemInformationImplementation::Qu
+@@ -3679,7 +3683,11 @@
+     // install ours
+     struct sigaction sa;
+     sa.sa_sigaction=(SigAction)StacktraceSignalHandler;
++#if defined(__minix)
++    sa.sa_flags=SA_RESTART|SA_RESETHAND;
++#else
+     sa.sa_flags=SA_SIGINFO|SA_RESTART|SA_RESETHAND;
++#endif
+     sigemptyset(&sa.sa_mask);
+ 
+     sigaction(SIGABRT,&sa,0);
+@@ -4616,7 +4624,7 @@ bool SystemInformationImplementation::Qu
    // a 32 bit process on a 64 bit host the returned memory will be
    // limited to 4GiB. So if this is a 32 bit process or if the sysconf
    // method fails use the kstat interface.
